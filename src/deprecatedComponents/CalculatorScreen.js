f8fbe3ee3ecff2bf895560807ccdf4408a07ad55
Azahar2 <a.zahar@baro-pfannenstein.de>
2021-02-01 22:20:45 +0100

    useState logic for the calculator

--- src/deprecatedComponents/CalculatorScreen.js
+++ src/deprecatedComponents/CalculatorScreen.js
@@ -1,36 +1,23 @@
-import React, { useReducer } from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "change_increment":
-      return { ...state, counter: state.counter + 1 };
-    // break;
-    case "change_decrement":
-      return { ...state, counter: state.counter - action.payload };
-    default:
-      return state;
-  }
-};
-const VALUE_INCREMENT = 1;
 const ScreenCalculator = () => {
-  const [state, dispatch] = useReducer(reducer, { counter: 0 });
-
+  const [counter, setCounter] = useState(0);
   return (
     <View>
       <Button
         title="Increment"
         onPress={() => {
-          dispatch({ type: "change_increment", payload: VALUE_INCREMENT });
+          setCounter(counter + 1);
         }}
       />
       <Button
         title="Decrement"
         onPress={() => {
-          dispatch({ type: "change_decrement", payload: -1 * VALUE_INCREMENT });
+          setCounter(counter - 1);
         }}
       />
-      <Text>Current Count: {state.counter}</Text>
+      <Text>Current Count: {counter}</Text>
     </View>
   );
 };
